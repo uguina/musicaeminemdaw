7b43980d69ae8738cc76a666e8a22382c20e95be
SergioSolanoMedina <sergio.solano@juanxxiii.net>
2022-01-26 12:11:05 +0100

    copiando js

--- js/jquery/G02/01_ready/script.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(){
-  alert('Bienvenido a KantamePlis. Disfruta nuestros contenidos!!');
-});
--- js/reprod.js
+++ js/reprod.js
@@ -1,40 +1,91 @@
-$(document).ready(() => {
-  const next = document.querySelector('.fa-forward');
-  const play = document.querySelector('.play-btn');
-  const pause = document.querySelector('.pause-btn');
-  const previous = document.querySelector('.fa-backward');
+
+const previous = document.querySelector('#previous');
+const next = document.querySelector('#next');
+const play = document.querySelector('#play').addEventListener('click', reproducir);;
+const pause = document.querySelector('#pause').addEventListener('click', pausar);;
+
+window.onload = cargarAjax;
+let canciones = [];
+function cargarAjax() {
+  cargar();
+  var req = new XMLHttpRequest();
+  req.open('GET', 'music.json', true);
+  req.send();
+  req.onreadystatechange = function () {
+    if (req.readyState == 4) {
+      if (req.status == 200) {
+        let datos = req.responseText;
+        canciones = JSON.parse(datos);
+        console.log(canciones);
+
+        canciones.forEach(cancion => {
+
+          let titulo = document.createElement('h3');
+          titulo.innerText = cancion.titulo;
+          document.querySelector("#lista-canciones").appendChild(titulo);
+
+          titulo.addEventListener("click", () => {
+
+            cargarCancion(cancion.id);
+          })
+
+        });
+      }
+    }
+  }
+}
+
+let reproductor = document.querySelector('#track');
+function cargarCancion(id) {
+  let titulo = canciones[id]['titulo'];
+  let cancion = canciones[id]['cancion'];
 
 
-  const song = document.querySelector('.player__audio');
-  const title = [];
-  var time;
+  document.querySelector('#cancion-seleccionada').innerHTML = titulo;
 
-  //PETICIÓN AJAX
-  // var req = new XMLHttpRequest();
-  // req.open('GET', 'music.json', true);
-  // req.send();
-  // req.onreadystatechange = function () {
-  //   if (req.readyState == 4) {
-  //     if (req.status == 200) {
-  //       let datos = req.responseText;
-  //       canciones = JSON.parse(datos);
+  reproductor.src = "./audio/" + cancion;
+  reproductor.play();
 
-  //       canciones.forEach(song => {
+  reproductor.addEventListener('timeupdate', function () {
+    document.querySelector('#barra').value = reproductor.currentTime;
 
-  //         let titulo = document.createElement('h3');
-  //         titulo.innerText = song.titulo;
-  //         document.querySelector("#lista-canciones").appendChild(titulo);
+    document.querySelector('#barra').max = reproductor.duration;
+  });
 
-  //         titulo.addEventListener("click", () => {
-  //           cargarCancion(song.id);
-  //         })
+  reproductor.addEventListener('ended', () => {
 
-  //       });
-  //     }
-  //   }
-  //}
+    if (id == canciones.length - 1) {
+      id = 0;
+      cargarCancion(id);
+    } else {
+      cargarCancion(++id);
+    }
 
-});
+  });
+}
+
+function cargar() {
+
+  document.querySelector('#play').addEventListener('click', reproducir);
+  document.querySelector('#pause').addEventListener('click', pausar);
+
+  document.querySelector('#barra').addEventListener('change', function () {
+    reproductor.currentTime = document.querySelector('#barra').value;
+
+  });
+
+}
+
+function reproducir() {
+  console.log('sonando');
+  reproductor.play();
+
+}
+
+function pausar() {
+  console.log('pausando');
+  reproductor.pause();
+}
 
 
 
